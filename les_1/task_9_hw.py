41fe278543f628a764a575d247cd2ccc4b314c70
Alexey Shevtsov <al913@mail.ru>
2023-08-03 17:05:12 +0300

    Lesson 1 - almost finish

--- les_1/task_9_hw.py
+++ les_1/task_9_hw.py
@@ -16,23 +16,35 @@ def index():
     return render_template('hw_index.html', title='Главная')
 
 
+@app.route('/about/')
+def about():
+    return render_template('hw_about.html', title='О нас')
+
+
 @app.route('/clothes/')
 def clothes():
     return render_template('hw_cloth.html', title='Одежда')
 
-@app.route('/about/')
-def about():
-    return render_template('hw_about.html', title='О нас')
+
+@app.route('/jackets/')
+def jackets():
+    return render_template('hw_jackets.html', title='Куртки')
+
+
+@app.route('/jacket/')
+def jacket():
+    return render_template('hw_jacket.html', title='Супер-куртка')
+
+
+@app.route('/development/')
+def develop():
+    return render_template('hw_develop.html', title='Страница в разработке')
 
 
 @app.route('/shoes/')
 def shoes():
     return render_template('hw_shoes.html', title='Обувь')
 
-
-@app.route('/jacket/')
-def jacket():
-    return render_template('hw_jacket.html', title='Куртка')
 
 if __name__ == '__main__':
     app.run(port=8900, debug=True)
