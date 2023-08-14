e1d1a874a945075bffe894458a5b2719b7cf97e2
Alexey <al913@mail.ru>
2023-08-15 00:14:52 +0300

    Lesson 4 HW

--- les_4/task_9.py
+++ les_4/task_9.py
@@ -24,31 +24,57 @@ urls = ['https://www.google.ru/',
         ]
 
 
-
-import re
 import requests
-from bs4 import BeautifulSoup
-
-site = 'http://pixabay.com'
-
-response = requests.get(site)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-img_tags = soup.find_all('img')
-
-urls = [img['src'] for img in img_tags]
+import os
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin, urlparse
 
 
-for url in urls:
-    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-    if not filename:
-         print("Regex didn't match with the url: {}".format(url))
-         continue
-    with open(filename.group(1), 'wb') as f:
-        if 'http' not in url:
-            # sometimes an image source can be relative 
-            # if it is provide the base url which also happens 
-            # to be the site variable atm. 
-            url = '{}{}'.format(site, url)
-        response = requests.get(url)
-        f.write(response.content)
+def is_valid(url):
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+
+def get_all_images(url):
+    soup = bs(requests.get(url).content, "html.parser")
+    urls = []
+    for img in tqdm(soup.find_all("img"), "Извлечение изображений"):
+        img_url = img.attrs.get("src")
+        if not img_url:
+            continue
+        img_url = urljoin(url, img_url)
+        try:
+            pos = img_url.index("?")
+            img_url = img_url[:pos]
+        except ValueError:
+            pass
+        if is_valid(img_url):
+            urls.append(img_url)
+    return urls
+
+
+def download(url, pathname):
+    if not os.path.isdir(pathname):
+        os.makedirs(pathname)
+    response = requests.get(url, stream=True)
+
+    file_size = int(response.headers.get("Content-Length", 0))
+
+    filename = os.path.join(pathname, url.split("/")[-1])
+
+    progress = tqdm(response.iter_content(1024), f"Загрузка {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        for data in progress.iterable:
+            f.write(data)
+            progress.update(len(data))
+
+
+def main(url, path):
+    imgs = get_all_images(url)
+    for img in imgs:
+        download(img, path)
+
+
+if __name__ == "__main__":
+    main('https://gb.ru/', f'{os.getcwd()}/images')
